3636a199187dab68b806bbecb8e92e813e125461
Afshan Khan <afshank2105@gmail.com>
2025-09-19 03:30:54 +0530

    clerk-middleware-change2

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-  runtime: "nodejs", // 👈 ADD THIS LINE
+  matcher: ["/basket(.*)"],
+  runtime: "nodejs",
 };
